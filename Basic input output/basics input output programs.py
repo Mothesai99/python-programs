86e09fcea303b2c591e1dc6e5ffeaad2533ccbaf
Mothe Saikrishna <108261487+Mothesai99@users.noreply.github.com>
2022-08-30 18:47:52 +0530

    initial commit

---
+++ Basic input output/basics input output programs.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #### 1.write a progrma to introduce yourself
+
+# In[1]:
+
+
+print('Hello')
+print('my name is saikrishna age=24 iam from karimnagar telangana state and i had recently completed my bachelor degree in electronics amd communication enginerring in the year 2021..and i had done some of mini projects and major projects and my hobbies are playing games watching movies and listening music my strength are self-motivated and hard working and weakness are iam perfect at work to present but i feel bit nervous that all about me')
+
+
+# #### 2.Write a program to perform the following operations:
+# - Addition
+# - Subtraction
+# - Multiplication
+# - Division
+# - Modulus
+# - Exponent
+
+# In[2]:
+
+
+num1=int(input('enter the number for a : '))
+num2=int(input('enter the number for b : '))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+mod=num1%num2
+exp=num1**num2
+print('The addition of two number is',add)
+print('The subtraction of two number is',sub)
+print('The multiplication of two number is',mul)
+print('The division of two number is',div)
+print('The modulus of two number is',mod)
+print('The Exponent of two number is',exp)
+
+
+# In[ ]:
+
+
+
+
